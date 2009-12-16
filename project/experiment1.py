4cdcddd91c6aa440f4ea0909e18e9f746d308908
Ivan Savov <ivan@web048012.wireless.mcgill.ca>
2009-12-16 09:47:55 -0500

    the new tex file.. moving work on it to qubit

--- project/experiment1.py
+++ project/experiment1.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+from mylibs import argparse
+from mylibs.ghettologging import gl
+from rungen import rungen
+
+import subprocess
+import json as simplejson
+
+import sys,os
+import random
+import re
+import tarfile
+import datetime
+
 #Number of topics
 #================
 #
@@ -11,3 +25,51 @@
 #    aprroximate runtime
 #calulate:
 #    perplexity
+
+PROJECT_PATH=os.path.realpath(os.path.join(os.path.dirname(__file__),".."))
+logfile = os.path.join(PROJECT_PATH,"project/results/exp1.log")
+resultdir = os.path.join(PROJECT_PATH,"project/results/")
+
+
+print "EXPERIMENT1 started....."
+
+
+gl("Building the run queue", logfile)
+
+runs = []
+counter = 109
+for T in [75, 100, 150, 200, 300]:
+    gl("Setting up iteration with T="+str(T), logfile)
+    counter +=1
+    r = rungen(counter, T=T, NITER=100, SEED=1)
+    runs.append(r)
+
+
+# run the runs !
+for run in runs:
+
+    gl("Starting run "+str(run["name"]), logfile)
+    gl("Run description: "+str(run), logfile)
+
+    start_time = datetime.datetime.now()
+    gl("start time: "+str(start_time), logfile)
+
+
+    (out,err)  = subprocess.Popen(run["runcommand"], shell=True, \
+                                               stdout=subprocess.PIPE, \
+                                               stderr=subprocess.PIPE \
+                                               ).communicate()
+
+    end_time = datetime.datetime.now()
+    gl("end time: "+str(end_time), logfile)
+
+    duration = (end_time-start_time).seconds
+    gl("run duration: "+str(duration)+" seconds", logfile)
+
+    #
+    resultfile = resultdir+str(run["name"])+"_results.json"
+    result=run
+    result["duration"]=duration
+
+    simplejson.dump(result, open(resultfile,"w") )
+
--- project/sparse.py
+++ project/sparse.py
@@ -1,23 +1,65 @@
 
 
+#% purpose: print topics from topic model run
+#% usage:   matlab> topics
+#% inputs:  wp.txt, vocab.txt
+#% outputs: topics.txt
+
+#[wid,tid,cnt] = textread('wp.txt','%d%d%d','headerlines',3);
+#wp = sparse(wid,tid,cnt);
 col=[]
 row=[]
-data=[]
-
+datacol=[]
 for line in cols:
     if len(line)>0:
         [i,j,d]= line.split()
         row.append(int(i)-1)
         col.append(int(j)-1)
-        data.append(int(d))
+        datacol.append(int(d))
+msparse = sparse.csc_matrix( (datacol, (row,col)), dtype=float)
+wp = msparse.toarray()
 
-msparse = sparse.csc_matrix( (data, (row,col)) )
 
-m = msparse.toarray()
+#[W,T] = size(wp);
 
+[W,T] = m.shape
+
+#beta = 0.01;
+#wp = wp + beta;
 beta = 0.01;
+wp += beta;
 
-m += beta;
 
 
+#fac = 1 ./ sum(wp,1);
+#prob_w_given_t = wp * diag(fac);
+
+fac = 1/sum(wp,0)
+prob_w_given_t = dot(wp, diag(fac) )
+
+
+#prob_t = sum(wp,1);
+#prob_t = prob_t / sum(prob_t);
+#
+#fid = fopen('topics2.txt','w');
+#[word] = textread('vocab.txt','%s');
+#if (length(word)~=W), error('bad size'); end;
+#
+#[xxsort,iisort] = sort(-prob_t);
+#
+#for tt=1:T
+#  %t = iisort(tt);
+#  t = tt;
+#  prob = prob_w_given_t(:,t);
+#  [xsort,isort] = sort(-prob);
+#  %fprintf(fid,'[t%d] (prob=%.3f) ', t, prob_t(t));
+#  fprintf(fid,'[t%d] ', t);
+#  for w=1:12
+#    fprintf(fid,'%s ', word{isort(w)});
+#  end
+#  fprintf(fid,'...\n');
+#end
+#
+#fclose(fid);
+#fprintf('wrote file topics2.txt\n');
 
